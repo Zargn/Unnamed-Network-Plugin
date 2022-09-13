2ab9a6c0c64af0041d1f357b7a11d198ac496ee0
Oskar Anderson <oskaranderson99@gmail.com>
2022-09-13 14:56:28 +0200

    Created unit test project

---
+++ Unnamed Networking Plugin Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Unnamed_Networking_Plugin_Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
