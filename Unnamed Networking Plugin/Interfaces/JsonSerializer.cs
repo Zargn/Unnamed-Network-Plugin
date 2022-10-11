c59874ac88cca0e77f8e6ef416424a2cdb587a8f
Oskar Anderson <oskaranderson99@gmail.com>
2022-10-11 15:47:18 +0200

    Created interfaces for future needed classes.

---
+++ Unnamed Networking Plugin/Interfaces/JsonSerializer.cs
@@ -0,0 +1,9 @@
+﻿namespace Unnamed_Networking_Plugin.Interfaces;
+
+public interface IJsonSerializer
+{
+    public string? Serialize<T>(T serializableClass);
+
+    public T? DeSerialize<T>(string json);
+    public object Deserialize(string json, Type type);
+}
