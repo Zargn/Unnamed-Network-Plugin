f9af2f12e796a2774b719fceb9f5d20b7d9dbea8
Oskar Anderson <oskaranderson99@gmail.com>
2022-10-24 20:40:03 +0200

    Create IdentificationPackage.cs

---
+++ Unnamed Networking Plugin/IdentificationPackage.cs
@@ -0,0 +1,13 @@
+﻿using Unnamed_Networking_Plugin.Interfaces;
+
+namespace Unnamed_Networking_Plugin;
+
+public class IdentificationPackage : Package
+{
+    public IConnectionInformation ConnectionInformation;
+
+    public IdentificationPackage(IConnectionInformation connectionInformation)
+    {
+        ConnectionInformation = connectionInformation;
+    }
+}
