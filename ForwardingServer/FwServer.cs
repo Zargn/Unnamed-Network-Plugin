b16efc89c3cb0ba49b7a90a5a910a88f54d01512
Oskar Anderson <oskaranderson99@gmail.com>
2022-10-31 19:50:36 +0100

    Began work on Server class

--- ForwardingServer/FwServer.cs
+++ ForwardingServer/FwServer.cs
@@ -6,17 +6,33 @@ namespace ForwardingServer;
 
 public class FwServer
 {
+    private int port { get; }
+    private ILogger logger { get; }
+    private IJsonSerializer jsonSerializer { get; }
+    private IdentificationPackage identificationPackage { get; }
+    public bool Running { get; private set; }
+
     public FwServer(int port, ILogger logger, IJsonSerializer jsonSerializer, IdentificationPackage identificationPackage)
     {
-        var client = new UnnamedNetworkPluginClient(port, logger, jsonSerializer, identificationPackage);
+        this.port = port;
+        this.logger = logger;
+        this.jsonSerializer = jsonSerializer;
+        this.identificationPackage = identificationPackage;
     }
 
     public async Task Run()
     {
+        if (Running)
+        {
+            logger.Log(this, "Run was called on an already running server. That does not work.", LogType.Error);
+            throw new Exception("Run was called on an already running server. This is not allowed.")
+        }
         
+        Running = true;
+        var client = new UnnamedNetworkPluginClient(port, logger, jsonSerializer, identificationPackage);
     }
 
-    public void Stop()
+    public async Task Stop()
     {
         
     }
