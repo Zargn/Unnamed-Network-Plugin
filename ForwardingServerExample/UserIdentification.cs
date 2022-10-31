577b5e9e9578ddf1aee0c54e862d6a75becd3080
Oskar Anderson <oskaranderson99@gmail.com>
2022-10-31 17:18:33 +0100

    Create UserIdentification.cs

---
+++ ForwardingServerExample/UserIdentification.cs
@@ -0,0 +1,46 @@
+﻿using Unnamed_Networking_Plugin.Interfaces;
+using Unnamed_Networking_Plugin.Resources;
+
+namespace ForwardingServerExample;
+
+public class UserIdentificationPackage : IdentificationPackage
+{
+    public UserIdentification UserIdentification { get; init; }
+
+    public UserIdentificationPackage(UserIdentification userIdentification)
+    {
+        UserIdentification = userIdentification;
+    }
+
+    public override IConnectionInformation? ExtractConnectionInformation()
+    {
+        return UserIdentification;
+    }
+}
+
+public class UserIdentification : IConnectionInformation
+{
+    public string UserName { get; init; }
+
+    public UserIdentification(string userName)
+    {
+        UserName = userName;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is UserIdentification)
+            return (obj as UserIdentification).UserName == UserName;
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return UserName;
+    }
+
+    public override int GetHashCode()
+    {
+        return UserName.GetHashCode();
+    }
+}
