43a96b3cefe58ec5d12fe1645f0b82acff58cde2
Oskar Anderson <oskaranderson99@gmail.com>
2022-10-31 17:29:05 +0100

    Began setting up forwarding server.

---
+++ ForwardingServer/FwServer.cs
@@ -0,0 +1,23 @@
+﻿using Unnamed_Networking_Plugin;
+using Unnamed_Networking_Plugin.Interfaces;
+using Unnamed_Networking_Plugin.Resources;
+
+namespace ForwardingServer;
+
+public class FwServer
+{
+    public FwServer(int port, ILogger logger, IJsonSerializer jsonSerializer, IdentificationPackage identificationPackage)
+    {
+        var client = new UnnamedNetworkPluginClient(port, logger, jsonSerializer, identificationPackage);
+    }
+
+    public async Task Run()
+    {
+        
+    }
+
+    public void Stop()
+    {
+        
+    }
+}
