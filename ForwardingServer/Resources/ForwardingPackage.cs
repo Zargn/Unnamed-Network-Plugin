c16f7ff5c6b4e474abf5c047c1dc2c84f0379bf8
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-01 11:43:43 +0100

    Create ForwardingPackage.cs

---
+++ ForwardingServer/Resources/ForwardingPackage.cs
@@ -0,0 +1,22 @@
+﻿using Unnamed_Networking_Plugin.Interfaces;
+using Unnamed_Networking_Plugin.Resources;
+
+namespace ForwardingServer;
+
+/// <summary>
+/// Forwarding Package class. Wraps a pre-made json string in a package together with targetInformation for the
+/// destination.
+/// </summary>
+/// <typeparam name="T">IConnectionInformation type.</typeparam>
+public class ForwardingPackage<T> : Package where T : IConnectionInformation
+{
+    public T TargetInformation { get; init; }
+    
+    public string PackageJson { get; init; }
+    
+    public ForwardingPackage(string packageJson, T targetInformation)
+    {
+        PackageJson = packageJson;
+        TargetInformation = targetInformation;
+    }
+}
