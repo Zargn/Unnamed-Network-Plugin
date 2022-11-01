e635b9c9fc2416be04e75181df2e379d27a9e817
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-01 17:44:23 +0100

    Started work on ServerInterface class

---
+++ ForwardingServer/ServerInterface.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using ForwardingServer.Resources;
+using Unnamed_Networking_Plugin;
+using Unnamed_Networking_Plugin.EventArgs;
+using Unnamed_Networking_Plugin.Resources;
+
+namespace ForwardingServer;
+
+public class ServerInterface
+{
+    private List<ConnectionGroup> connectionGroups;
+    private UnnamedNetworkPluginClient client;
+
+    public ServerInterface(List<ConnectionGroup> connectionGroups, UnnamedNetworkPluginClient client)
+    {
+        this.connectionGroups = connectionGroups;
+        this.client = client;
+    }
+
+    public void HandleListGroupsPackage(PackageReceivedEventDetailedArgs args)
+    {
+        List<GroupInformation> groupInformationList = connectionGroups.Select(connectionGroup => connectionGroup.GroupInformation).ToList();
+        
+        var sendPackageTask = client.SendPackage(new GroupsListPackage(groupInformationList), args.ConnectionInformation);
+        
+        sendPackageTask.Wait();
+    }
+}
