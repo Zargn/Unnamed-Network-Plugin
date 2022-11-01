cfadab0cb9d45628f0a6502702650892d807906d
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-01 11:44:09 +0100

    FwServer.cs: Prepared fields for ForwardingPackage

--- ForwardingServer/FwServer.cs
+++ ForwardingServer/FwServer.cs
@@ -10,22 +10,28 @@ public class FwServer
     private ILogger logger { get; }
     private IJsonSerializer jsonSerializer { get; }
     private IdentificationPackage identificationPackage { get; }
+    private Type identificationType { get; }
     public bool Running { get; private set; }
 
-    public FwServer(int port, ILogger logger, IJsonSerializer jsonSerializer, IdentificationPackage identificationPackage)
+    public FwServer(
+        int port, 
+        ILogger logger, 
+        IJsonSerializer jsonSerializer, 
+        IdentificationPackage identificationPackage)
     {
         this.port = port;
         this.logger = logger;
         this.jsonSerializer = jsonSerializer;
         this.identificationPackage = identificationPackage;
+        identificationType = identificationPackage.ExtractConnectionInformation().GetType();
     }
-
+    
     public async Task Run()
     {
         if (Running)
         {
             logger.Log(this, "Run was called on an already running server. That does not work.", LogType.Error);
-            throw new Exception("Run was called on an already running server. This is not allowed.")
+            throw new Exception("Run was called on an already running server. This is not allowed.");
         }
         
         Running = true;
