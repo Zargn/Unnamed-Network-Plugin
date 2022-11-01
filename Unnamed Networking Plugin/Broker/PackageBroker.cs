67ab034c8486b1c80d0000bbe35e86b22226752e
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-01 16:48:40 +0100

    Finished broker class for now.

--- Unnamed Networking Plugin/Broker/PackageBroker.cs
+++ Unnamed Networking Plugin/Broker/PackageBroker.cs
@@ -1,26 +1,45 @@
-﻿using Unnamed_Networking_Plugin.Interfaces;
+﻿using Unnamed_Networking_Plugin.Resources;
 
 namespace Unnamed_Networking_Plugin.Broker;
 
 public class PackageBroker
 {
-    public static void ConfigureBrokerPackageIds(IEnumerable<IBrokerPackage> brokerPackages)
+    // private Action<IBrokerPackage>[] listeners;
+    private readonly Dictionary<Type, Action<Package>?> listeners = new();
+    public EventHandler<Package>? PackageWithNoSubscribersReceived;
+
+    // public static void ConfigureBrokerPackageIds(IEnumerable<IBrokerPackage> brokerPackages)
+    // {
+    //     
+    // }
+    
+    // TODO. Look into making this class more efficient.
+
+    public void SubscribeToPackage<T>(Action<Package> onPackageReceived) where T : Package
     {
-        
+        if (listeners.TryGetValue(typeof(T), out var listener))
+        {
+            listeners[typeof(T)] += onPackageReceived;
+            return;
+        }
+        listeners[typeof(T)] = onPackageReceived;
     }
 
-    public static void SubscribeToPackage<IBrokerPackage>(Action<IBrokerPackage> onPackageReceived)
+    public void UnSubscribeFromPackage<T>(Action<Package> onPackageReceived) where T : Package
     {
-        
+        if (listeners.TryGetValue(typeof(T), out var listener))
+        {
+            listeners[typeof(T)] -= onPackageReceived;
+        }
     }
 
-    public static void UnSubscribeFromPackage<IBrokerPackage>(Action<IBrokerPackage> onPackageReceived)
+    public void InvokeSubscribers(Package package, Type type)
     {
-        
-    }
-
-    public void InvokeSubscribers(IBrokerPackage package)
-    {
-        
+        if (listeners.TryGetValue(type, out var listener))
+        {
+            listener?.Invoke(package);
+            return;
+        }
+        PackageWithNoSubscribersReceived?.Invoke(this, package);
     }
 }
