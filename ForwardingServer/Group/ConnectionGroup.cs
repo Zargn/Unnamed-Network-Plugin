51124f7d00fd6160d4aea6ccd9ce25d82e714f1e
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-02 16:22:59 +0100

    Continued work on connectionGroup

--- ForwardingServer/Group/ConnectionGroup.cs
+++ ForwardingServer/Group/ConnectionGroup.cs
@@ -1,27 +1,74 @@
-﻿using Unnamed_Networking_Plugin;
+﻿using ForwardingServer.Group.Resources;
+using Unnamed_Networking_Plugin;
 using Unnamed_Networking_Plugin.Broker;
+using Unnamed_Networking_Plugin.EventArgs;
+using Unnamed_Networking_Plugin.Interfaces;
+using Unnamed_Networking_Plugin.Resources;
 
-namespace ForwardingServer;
+namespace ForwardingServer.Group;
 
 public class ConnectionGroup
 {
-    public GroupInformation GroupInformation => new GroupInformation()
+    public GroupInformation GroupInformation => new GroupInformation(
+        groupSettings.MaxSize, groupSettings.Title,
+        groupSettings.Description, MemberCount);
     public PackageBroker Broker { get; } = new();
-
+    public int MemberCount => members.Count;
+    
     private readonly GroupSettings groupSettings;
     private List<Connection> members = new();
+    private IdentificationPackage package;
+    private Type forwardingPackageType;
+    private UnnamedNetworkPluginClient client;
 
-    public ConnectionGroup(GroupSettings groupSettings)
+    public ConnectionGroup(GroupSettings groupSettings, Type forwardingPackageType, UnnamedNetworkPluginClient client)
     {
         this.groupSettings = groupSettings;
+        this.forwardingPackageType = forwardingPackageType;
+        this.client = client;
     }
 
     public bool Join(Connection connection)
     {
+        if (MemberCount >= groupSettings.MaxSize)
+            return false;
+
+        // connection.PackageReceived += Broker.InvokeSubscribers;
         
+        return true;
     }
 
     public void Leave(Connection connection)
+    {
+        
+    }
+
+    private void SetUpSubscribers()
+    {
+        Broker.SubscribeToPackage(HandleForwardingPackage, forwardingPackageType);
+        Broker.SubscribeToPackage<RequestGroupInformationPackage>(HandleRequestGroupInformationPackage);
+        Broker.SubscribeToPackage<LeaveGroupPackage>(HandleLeaveGroupPackage);
+    }
+
+    private void HandleForwardingPackage(object? o, PackageReceivedEventDetailedArgs args)
+    {
+        var package = args.ReceivedPackage as ForwardingPackage;
+        var targetInfo = package.TargetInformation as IConnectionInformation;
+        
+        // Todo: Send package.PackageJson to targetInfo
+    }
+
+    private void ForwardPackageToAll(object? o, PackageReceivedEventDetailedArgs args)
+    {
+        
+    }
+
+    private void HandleRequestGroupInformationPackage(object? o, PackageReceivedEventDetailedArgs args)
+    {
+        
+    }
+
+    private void HandleLeaveGroupPackage(object? o, PackageReceivedEventDetailedArgs args)
     {
         
     }
