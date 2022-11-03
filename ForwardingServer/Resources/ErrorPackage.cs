19f31ec64fa6006d3311053230077971a2dd320f
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-03 11:31:07 +0100

    Created Warning and Error packages.
    These will inform connected clients about problems from the server.

---
+++ ForwardingServer/Resources/ErrorPackage.cs
@@ -0,0 +1,15 @@
+﻿using Unnamed_Networking_Plugin.Resources;
+
+namespace ForwardingServer.Resources;
+
+public class ErrorPackage : Package
+{
+    public string ErrorMessage { get; init; }
+    public Exception Exception { get; init; }
+
+    public ErrorPackage(string errorMessage, Exception exception)
+    {
+        ErrorMessage = errorMessage;
+        Exception = exception;
+    }
+}
