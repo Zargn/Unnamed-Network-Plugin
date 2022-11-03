a17f63e3caf793a61b017a6f4b81cebeba7813f2
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-03 12:14:38 +0100

    ServerInterface.cs: Continued work.
    Now has a hopefully working handleJoinGroup method.

--- ForwardingServer/ServerInterface.cs
+++ ForwardingServer/ServerInterface.cs
@@ -26,14 +26,18 @@ public class ServerInterface
         SetUpSubscribers();
     }
 
-    public void PutInMenu(Connection connection)
+    public async Task PutInMenu(Connection connection)
     {
-        
+        var sendPackageTask = connection.SendPackage(new InMenuPackage());
+        connection.PackageReceived += Broker.InvokeSubscribers;
+        connection.ClientDisconnected += HandleClientDisconnected;
+        await sendPackageTask;
     }
 
     public void RemoveFromMenu(Connection connection)
     {
-        
+        connection.PackageReceived -= Broker.InvokeSubscribers;
+        connection.ClientDisconnected -= HandleClientDisconnected;
     }
 
     private void SetUpSubscribers()
@@ -43,6 +47,12 @@ public class ServerInterface
         Broker.SubscribeToPackage<JoinGroupPackage>(HandleJoinGroupPackage);
     }
 
+    private void HandleClientDisconnected(object? o, ClientDisconnectedEventArgs args)
+    {
+        var connection = args.Connection;
+        RemoveFromMenu(connection);
+    }
+    
     private async void HandleListGroupsPackage(object? o, PackageReceivedEventArgs args)
     {
         var groupInformationList = connectionGroups.Select(pair => pair.Value.GroupInformation).ToList();
@@ -53,9 +63,16 @@ public class ServerInterface
     private async void HandleCreateGroupPackage(object? o, PackageReceivedEventArgs args)
     {
         await client.SendPackage(new RequestGroupSettingsPackage(), args.ConnectionInformation);
+        
+        
+        
+        // TODO: Finish this....
+        
+        
+        
     }
 
-    private void HandleJoinGroupPackage(object? o, PackageReceivedEventArgs args)
+    private async void HandleJoinGroupPackage(object? o, PackageReceivedEventArgs args)
     {
         var connection = client.GetConnectionFromList(args.ConnectionInformation);
         RemoveFromMenu(connection);
@@ -63,5 +80,26 @@ public class ServerInterface
         var package = args.ReceivedPackage as JoinGroupPackage;
 
         // Todo: Get group with matching package.TargetGroupInformation then attempt to join.
+        try
+        {
+            var targetGroup = connectionGroups[package.TargetGroupInformation.GroupSettings];
+            if (await targetGroup.Join(connection))
+            {
+                RemoveFromMenu(connection);
+                return;
+            }
+            
+            string warningMessage = "Provided group was full.";
+            var warningPackage = new WarningPackage(warningMessage, WarningType.GroupFull);
+            await connection.SendPackage(warningPackage);
+        }
+        catch (KeyNotFoundException)
+        {
+            string warningMessage = "Provided group information does not exist on this server.";
+            var warningPackage = new WarningPackage(warningMessage, WarningType.GroupNotFound);
+            await connection.SendPackage(warningPackage);
+            throw;
+        }
+        
     }
 }
