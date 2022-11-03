3f20ab72fb1320c86b40f72449806fbea7bf915c
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-03 17:41:45 +0100

    Program.cs: Created todo list.

--- ForwardingClientExample/Program.cs
+++ ForwardingClientExample/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace ForwardingClientExample;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main()
+    {
+        // Todo: use the FwClient to connect to a forwarding server and subscribe to all events.
+        
+        // Create a / based command system where /COMMAND is used to trigger special events.
+        
+        // Any regular non-command message should be forwarded to all clients in the current group.
+    }
+}
