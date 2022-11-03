e76cbc5426cbc9a398c0a1761825d4aa45ea03ea
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-03 17:38:52 +0100

    FwClient.cs: Implemented methods.

--- ForwardingClient/FwClient.cs
+++ ForwardingClient/FwClient.cs
@@ -1,7 +1,11 @@
 ﻿using System.Net;
+using System.Threading.Channels;
 using ForwardingClient.Resources;
+using ForwardingClient.Resources.CommandPackages;
+using ForwardingClient.Resources.ForwardingPackages;
 using Unnamed_Networking_Plugin;
 using Unnamed_Networking_Plugin.Broker;
+using Unnamed_Networking_Plugin.EventArgs;
 using Unnamed_Networking_Plugin.Interfaces;
 using Unnamed_Networking_Plugin.Resources;
 
@@ -16,6 +20,7 @@ public class FwClient
     private IJsonSerializer JsonSerializer { get; }
     private IdentificationPackage IdentificationPackage { get; }
     private UnnamedNetworkPluginClient Client { get; set; }
+    private Connection Connection { get; set; }
     private bool inMenu;
     
     public bool Connected { get; private set; }
@@ -29,33 +34,57 @@ public class FwClient
     
     public FwClient(ILogger logger, IJsonSerializer jsonSerializer, IdentificationPackage identificationPackage)
     {
-        Port = Port;
         Logger = logger;
         JsonSerializer = jsonSerializer;
         IdentificationPackage = identificationPackage;
         PackageBroker = new PackageBroker();
     }
-    
-    
 
-    public async Task ConnectAsync(IPAddress ipAddress, int port)
+
+
+    public async Task<bool> ConnectAsync(IPAddress ipAddress, int port)
     {
-        throw new NotImplementedException();
+        // throw new NotImplementedException();
+
+        Client = new UnnamedNetworkPluginClient(port, Logger, JsonSerializer, IdentificationPackage);
+        
+        // TODO: This is only here until a way to start the Client without the listener active is created.
+        var stopListenerTask = Client.StopListener();
+
+        Client.ConnectionSuccessful += HandleConnectionSuccessful;
+
+        var connectTask = Client.AddConnection(ipAddress, port);
+        return await connectTask;
     }
 
+    private void HandleConnectionSuccessful(object? o, ConnectionReceivedEventArgs args)
+    {
+        args.Connection.PackageReceived += PackageBroker.InvokeSubscribers;
+        args.Connection.ClientDisconnected += HandleClientDisconnected;
+
+        Connection = args.Connection;
+    }
+
+    private void HandleClientDisconnected(object? o, ClientDisconnectedEventArgs args)
+    {
+        
+    }
+
+    
+    
     public async Task DisconnectAsync()
     {
-        throw new NotImplementedException();
+        Connection.Disconnect();
     }
 
     public async Task SendListGroupsRequest()
     {
-        throw new NotImplementedException();
+        await Connection.SendPackage(new RequestGroupsListPackage());
     }
 
     public async Task SendCreateGroupRequest(GroupSettings groupSettings)
     {
-        throw new NotImplementedException();
+        await Connection.SendPackage(new CreateGroupPackage(groupSettings));
     }
 
     public async Task SendCreateGroupRequest(int maxSize, string title, string description)
@@ -65,7 +94,7 @@ public class FwClient
 
     public async Task SendJoinGroupRequest(GroupSettings groupSettings)
     {
-        throw new NotImplementedException();
+        await Connection.SendPackage(new JoinGroupPackage(groupSettings));
     }
 
     public async Task SendJoinGroupRequest(int maxSize, string title, string description)
@@ -76,21 +105,23 @@ public class FwClient
     public async Task SendPackageToGroupMember<T>(T package, IConnectionInformation connectionInformation)
         where T : Package
     {
-        throw new NotImplementedException();
+        var packageJson = JsonSerializer.Serialize(package);
+        await Connection.SendPackage(new ForwardingPackage(packageJson, connectionInformation));
     }
 
     public async Task SendPackageToAllGroupMembers<T>(T package) where T : Package
     {
-        throw new NotImplementedException();
+        var packageJson = JsonSerializer.Serialize(package);
+        await Connection.SendPackage(new ForwardingPackageAll(packageJson));
     }
 
     public async Task SendGroupInformationRequest()
     {
-        throw new NotImplementedException();
+        await Connection.SendPackage(new RequestGroupInformationPackage());
     }
 
     public async Task SendLeaveGroupRequest()
     {
-        throw new NotImplementedException();
+        await Connection.SendPackage(new LeaveGroupPackage());
     }
 }
