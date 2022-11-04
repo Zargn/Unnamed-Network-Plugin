8ba56d1ae61c004b179d6a67cd4d928f27cf510c
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-04 10:29:20 +0100

    Created packages for notifying group joins/leaves

---
+++ ForwardingServer/Resources/InformationPackages/ClientJoinedGroupPackage.cs
@@ -0,0 +1,14 @@
+﻿using Unnamed_Networking_Plugin.Interfaces;
+using Unnamed_Networking_Plugin.Resources;
+
+namespace ForwardingServer.Resources.InformationPackages;
+
+public class ClientJoinedGroupPackage : Package
+{
+    public object ClientInformation { get; init; }
+
+    public ClientJoinedGroupPackage(IConnectionInformation clientInformation)
+    {
+        ClientInformation = clientInformation;
+    }
+}
---
+++ ForwardingServer/Resources/InformationPackages/ClientLeftGroupPackage.cs
@@ -0,0 +1,14 @@
+﻿using Unnamed_Networking_Plugin.Interfaces;
+using Unnamed_Networking_Plugin.Resources;
+
+namespace ForwardingServer.Resources.InformationPackages;
+
+public class ClientLeftGroupPackage : Package
+{
+    public object ConnectionInformation { get; init; }
+
+    public ClientLeftGroupPackage(IConnectionInformation connectionInformation)
+    {
+        ConnectionInformation = connectionInformation;
+    }
+}
