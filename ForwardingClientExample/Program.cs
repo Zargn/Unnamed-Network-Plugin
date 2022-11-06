88c75a390e8d24ec1100cb294e0a53dc8d69037e
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-06 18:57:25 +0100

    Program.cs: Configured most package subscribers

--- ForwardingClientExample/Program.cs
+++ ForwardingClientExample/Program.cs
@@ -1,7 +1,10 @@
-﻿using ForwardingClient;
+﻿using System.Text.RegularExpressions;
+using ForwardingClient;
 using ForwardingClientExample.Commands;
 using ForwardingClientExample.CommandSystem;
+using ForwardingServer.Resources;
 using ForwardingServer.Resources.CommandPackages;
+using ForwardingServer.Resources.InformationPackages;
 using ForwardingServerExampleShared;
 using Unnamed_Networking_Plugin;
 
@@ -63,6 +66,7 @@ public class Program
         commandFilter = new CommandFilter(disconnectedCommands);
 
         SubscribeToPackages();
+        fwClient.ClientDisconnected += HandleClientDisconnected;
     }
     
     public void Run()
@@ -84,12 +88,78 @@ public class Program
     private void SubscribeToPackages()
     {
         fwClient.PackageBroker.SubscribeToPackage<InMenuPackage>(HandleInMenuPackage);
+        fwClient.PackageBroker.SubscribeToPackage<GroupsListPackage>(HandleGroupsListPackage);
+        fwClient.PackageBroker.SubscribeToPackage<InGroupPackage>(HandleInGroupPackage);
+        fwClient.PackageBroker.SubscribeToPackage<ClientJoinedGroupPackage>(HandleClientJoinedGroupPackage);
+        fwClient.PackageBroker.SubscribeToPackage<ClientLeftGroupPackage>(HandleClientLeftGroupPackage);
+        fwClient.PackageBroker.SubscribeToPackage<GroupInformationPackage>(HandleGroupInformationPackage);
+        fwClient.PackageBroker.SubscribeToPackage<WarningPackage>(HandleWarningPackage);
+        fwClient.PackageBroker.SubscribeToPackage<ErrorPackage>(HandleErrorPackage);
     }
 
-    private async void HandleInMenuPackage(object? o, PackageReceivedEventArgs args)
+    
+    // Menu
+    private void HandleInMenuPackage(object? o, PackageReceivedEventArgs args)
     {
         ConnectionState = ConnectionState.ConnectedInMenu;
         commandFilter.SetCommandList(menuCommands);
+    }
+
+    private void HandleGroupsListPackage(object? o, PackageReceivedEventArgs args)
+    {
+        var package = args.ReceivedPackage as GroupsListPackage;
+        foreach (var groupInformation in package.GroupInformation)
+        {
+            Console.WriteLine(groupInformation);
+        }
+    }
+    
+    // Group
+    private void HandleInGroupPackage(object? o, PackageReceivedEventArgs args)
+    {
+        ConnectionState = ConnectionState.ConnectedInGroup;
+        commandFilter.SetCommandList(groupCommands);
+    }
+    
+    private void HandleClientJoinedGroupPackage(object? o, PackageReceivedEventArgs args)
+    {
+        var package = args.ReceivedPackage as ClientJoinedGroupPackage;
+        Console.WriteLine($"{package.ClientInformation} Joined the group.");
+    }
+    
+    private void HandleClientLeftGroupPackage(object? o, PackageReceivedEventArgs args)
+    {
+        var package = args.ReceivedPackage as ClientLeftGroupPackage;
+        Console.WriteLine($"{package.ConnectionInformation} Left the group.");
+    }
+    
+    private void HandleGroupInformationPackage(object? o, PackageReceivedEventArgs args)
+    {
+        var package = args.ReceivedPackage as GroupInformationPackage;
+        Console.WriteLine("Current group:");
+        Console.WriteLine(package.GroupInformation);
+    }
+    
+    // General
+    private void HandleWarningPackage(object? o, PackageReceivedEventArgs args)
+    {
+        var package = args.ReceivedPackage as WarningPackage;
+        Console.WriteLine($"The remote server answered with the following warning:");
+        Console.WriteLine($"[{package.WarningType.ToString()}]: {package.WarningMessage}");
+    }
+    
+    private void HandleErrorPackage(object? o, PackageReceivedEventArgs args)
+    {
+        var package = args.ReceivedPackage as ErrorPackage;
+        Console.WriteLine($"The remote server experienced the following error upon your request:");
+        Console.WriteLine($"[{package.ErrorMessage}]:");
+        Console.WriteLine(package.Exception);
+    }
+
+    private void HandleClientDisconnected(object? o, EventArgs args)
+    {
+        ConnectionState = ConnectionState.Disconnected;
+        commandFilter.SetCommandList(disconnectedCommands);
     }
 }
 
