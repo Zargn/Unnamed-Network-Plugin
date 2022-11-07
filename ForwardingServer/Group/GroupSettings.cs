b48912710f222078502eec51dc98f8b6aec4fdf4
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-07 19:01:00 +0100

    GroupSettings.cs: Now compares only titles.

--- ForwardingServer/Group/GroupSettings.cs
+++ ForwardingServer/Group/GroupSettings.cs
@@ -1,3 +1,11 @@
 ﻿namespace ForwardingServer;
 
-public record GroupSettings(int MaxSize, string Title, string Description);
+public record GroupSettings(int MaxSize, string Title, string Description)
+{
+    public virtual bool Equals(GroupSettings? other)
+    {
+        if (other == null)
+            return false;
+        return Title == other.Title;
+    }
+}
