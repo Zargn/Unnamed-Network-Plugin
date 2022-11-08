557d4344385169efe16c1b55cd9de1f2c2beb6e7
Oskar Anderson <oskaranderson99@gmail.com>
2022-11-08 18:54:54 +0100

    MessagePackage.cs: Created

---
+++ ForwardingClientExample/MessagePackage.cs
@@ -0,0 +1,16 @@
+﻿using ForwardingServerExampleShared;
+using Unnamed_Networking_Plugin.Resources;
+
+namespace ForwardingClientExample;
+
+public class MessagePackage : Package
+{
+    public string Message { get; init; }
+    public UserIdentification Sender { get; init; }
+    
+    public MessagePackage(string message, UserIdentification sender)
+    {
+        Message = message;
+        Sender = sender;
+    }
+}
